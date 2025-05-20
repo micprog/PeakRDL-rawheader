4d15a9dc308329f65957112cf1cad72d96dfbccd
Michael Rogenmoser <michael@rogenmoser.us>
2025-05-20 15:04:56 +0200

    Add license headers

--- src/peakrdl_rawheader/__peakrdl__.py
+++ src/peakrdl_rawheader/__peakrdl__.py
@@ -1,5 +1,9 @@
-# __peakrdl__.py
-# PeakRDL Exporter Plugin: Header Generator using Mako templates
+#!/usr/bin/env python3
+# Copyright 2025 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Author: Michael Rogenmoser <michaero@iis.ee.ethz.ch>
 
 import os
 from itertools import product
